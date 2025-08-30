9aa254ece321807c74ace8ad5e2b0e92d775addd
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-08-31 02:48:35 +0530

    first commit

---
+++ database/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => 
+{
+  mongoose.connection.on(() => {
+    console.log("Database connected") ;
+  })
+   mongoose.connect(process.env.MongoDb_URI) ;
+}
+
+export default connectDb ;
---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middlewares
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:3000", // your frontend URL
+  credentials: true
+}));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hii from server 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server started at ${PORT}`);
+});
